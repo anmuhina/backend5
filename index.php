1b8db3faba9ac114cc5962aeda7fb7f696ce4291
anmuhina <112845722+anmuhina@users.noreply.github.com>
2023-04-12 19:09:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -89,19 +89,42 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
      $db = new PDO('mysql:host=localhost;dbname=u52811', $user, $pass,
        [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); 
      
-     $stmt = $db->prepare("SELECT name, email, birth_date, sex, amount_of_limbs, abilities, biography, informed from application1 join application-ability on (application1.id=application_ability.app_id) where login = ? and id=?");
-     $result = $stmt->execute([$_SESSION['login'],$_SESSION['uid']]);
-     $values['name', 'email', 'birth_date', 'sex', 'amount_of_limbs', 'abilities', 'biography', 'informed'] = $result->fetchAll();
+     $stmt1 = $db->prepare("SELECT name from application1 where login = ? and id=?");
+     $res1 = $stmt1->execute([$_SESSION['login'],$_SESSION['uid']]);
+     $name = $res1->fetchAll();
+     $stmt2 = $db->prepare("SELECT email from application1 where login = ? and id=?");
+     $res2 = $stmt2->execute([$_SESSION['login'],$_SESSION['uid']]);
+     $email = $res2->fetchAll();
+     $stmt3 = $db->prepare("SELECT birth_date from application1 where login = ? and id=?");
+     $res3 = $stmt3->execute([$_SESSION['login'],$_SESSION['uid']]);
+     $birth_date = $res3->fetchAll();
+     $stmt4 = $db->prepare("SELECT sex from application1 where login = ? and id=?");
+     $res4 = $stmt4->execute([$_SESSION['login'],$_SESSION['uid']]);
+     $sex = $res4->fetchAll();
+     $stmt5 = $db->prepare("SELECT amount_of_limbs from application1 where login = ? and id=?");
+     $res5 = $stmt5->execute([$_SESSION['login'],$_SESSION['uid']]);
+     $amount_of_limbs = $res5->fetchAll();
+     $stmt6 = $db->prepare("SELECT ab_id from application1 join application_ability on (application1.id=application_ability.app_id) where login = ? and id=?");
+     $res6 = $stmt6->execute([$_SESSION['login'],$_SESSION['uid']]);
+     $abilities[] = $res6->fetchAll();
+     $stmt7 = $db->prepare("SELECT biography from application1 where login = ? and id=?");
+     $res7 = $stmt7->execute([$_SESSION['login'],$_SESSION['uid']]);
+     $biography = $res7->fetchAll();
+     $stmt8 = $db->prepare("SELECT informed from application1 where login = ? and id=?");
+     $res8 = $stmt8->execute([$_SESSION['login'],$_SESSION['uid']]);
+     $informed = $res8->fetchAll();
+    
+     $values (
+       ['name'] => strip_tags($name)
+       ['email'] => strip_tags($email)
+       ['birth_date'] => $birth_date
+       ['sex'] => $sex
+       ['amount_of_limbs'] => $amount_of_limbs
+       ['abilities'] => $abilities
+       ['biography'] => strip_tags($biography)
+       ['informed'] => $informed
+     )
      
-       /*$values['name'] = strip_tags(mysqli_query($db, "select name from application1 where login=$_SESSION['login']"));
-       $values['email'] = strip_tags(mysqli_query($db, "select email from application1 where login=$_SESSION['login']"));
-       $values['birth_date'] = mysqli_query($db, "select birth_date from application1 where login=$_SESSION['login']");
-       $values['sex'] = mysqli_query($db, "select sex from application1 where login=$_SESSION['login']");
-       $values['amount_of_limbs'] = mysqli_query($db, "select amount_of_limbs from application1 where login=$_SESSION['login']");
-       $values['abilities'] = mysqli_query($db, "select abilities from application1 where login=$_SESSION['login']");
-       $values['biography'] = strip_tags(mysqli_query($db, "select biography from application1 where login=$_SESSION['login']"));
-       $values['informed'] = mysqli_query($db, "select name from application1 where login=$_SESSION['login']");*/
-  
     printf('Вход с логином %s, uid %d', $_SESSION['login'], $_SESSION['uid']);
   }
   
