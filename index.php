fa50f6a2af0b9e93bb298c2854be312c3f18b34d
anmuhina <112845722+anmuhina@users.noreply.github.com>
2023-04-13 17:32:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -253,8 +253,8 @@ else {
     $uid=$_SESSION['uid'];
     
     try {
-    $stmt=$db->prepare("UPDATE application1 SET name = ?, email = ?, birth_date = ?, sex = ?, amount_of_limbs = ?, biography = ? where id=$uid"); 
-    $stmt -> execute([$_POST['name'], $_POST['email'], $_POST['birth_date'], $_POST['sex'], $_POST['amount_of_limbs'], $_POST['biography']]);
+    $stmt=$db->prepare("UPDATE application1 SET id = ?, name = ?, email = ?, birth_date = ?, sex = ?, amount_of_limbs = ?, biography = ? where id=$uid"); 
+    $stmt -> execute([$uid, $_POST['name'], $_POST['email'], $_POST['birth_date'], $_POST['sex'], $_POST['amount_of_limbs'], $_POST['biography']]);
     }
     catch (PDOException $e) {
        print('Error : ' . $e->getMessage());
