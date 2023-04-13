9f1d35efde02e8a4c9ec9e97d57cfa16c9eb1a0f
anmuhina <112845722+anmuhina@users.noreply.github.com>
2023-04-13 16:09:20 +0300

    Update login.php

--- login.php
+++ login.php
@@ -8,44 +8,15 @@ if (!empty($_SESSION['login'])) {
   // Если есть логин в сессии, то пользователь уже авторизован.
   // TODO: Сделать выход (окончание сессии вызовом session_destroy()
   //при нажатии на кнопку Выход)
-     $user = 'u52811';
-     $pass = '8150350';
-     $db = new PDO('mysql:host=localhost;dbname=u52811', $user, $pass,
-       [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); 
-     $stmt1 = $db->prepare("SELECT name from application1 where login = ? and id=?");
-     $res1 = $stmt1->execute([$_SESSION['login'],$_SESSION['uid']]);
-     $name = $res1->fetchAll();
-     $stmt2 = $db->prepare("SELECT email from application1 where login = ? and id=?");
-     $res2 = $stmt2->execute([$_SESSION['login'],$_SESSION['uid']]);
-     $email = $res2->fetchAll();
-     $stmt3 = $db->prepare("SELECT birth_date from application1 where login = ? and id=?");
-     $res3 = $stmt3->execute([$_SESSION['login'],$_SESSION['uid']]);
-     $birth_date = $res3->fetchAll();
-     $stmt4 = $db->prepare("SELECT sex from application1 where login = ? and id=?");
-     $res4 = $stmt4->execute([$_SESSION['login'],$_SESSION['uid']]);
-     $sex = $res4->fetchAll();
-     $stmt5 = $db->prepare("SELECT amount_of_limbs from application1 where login = ? and id=?");
-     $res5 = $stmt5->execute([$_SESSION['login'],$_SESSION['uid']]);
-     $amount_of_limbs = $res5->fetchAll();
-     $stmt6 = $db->prepare("SELECT ab_id from application1 join application_ability on (application1.id=application_ability.app_id) where login = ? and id=?");
-     $res6 = $stmt6->execute([$_SESSION['login'],$_SESSION['uid']]);
-     $abilities[] = $res6->fetchAll();
-     $stmt7 = $db->prepare("SELECT biography from application1 where login = ? and id=?");
-     $res7 = $stmt7->execute([$_SESSION['login'],$_SESSION['uid']]);
-     $biography = $res7->fetchAll();
-     $stmt8 = $db->prepare("SELECT informed from application1 where login = ? and id=?");
-     $res8 = $stmt8->execute([$_SESSION['login'],$_SESSION['uid']]);
-     $informed = $res8->fetchAll();
-     $stmt9 = $db->prepare("SELECT password from application1 where login = ? and id=?");
-     $res9 = $stmt9->execute([$_SESSION['login'],$_SESSION['uid']]);
-     $password = $res9->fetchAll();
-    
-     $values = ["name"=>strip_tags($name), "email"=>strip_tags($email), "birth_date"=>$birth_date, "amount_of_limbs"=>$amount_of_limbs, "abilities"=>$abilities, "biography"=>strip_tags($biography), "informed"=>$informed, "login"=>$_SESSION['login'], "password"=>$password];
      
-  
+  echo '<p> Поздравляем, вы уже авторизованы! </p>';
   // Делаем перенаправление на форму.
   header('Location: ./');
 }
+
+else {
+  
+  
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 ?>
@@ -110,4 +81,8 @@ else {
     
   header('Location: ./');
     
+}
+
+  
+  
 }
